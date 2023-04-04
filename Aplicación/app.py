3292d989c16832c6c87d66cbdf96272dca104574
GlobalAlumniData <129831851+GlobalAlumniData@users.noreply.github.com>
2023-04-04 09:30:32 +0200

    Código aplicación
    Subida de archivos .py con el código para crear la aplicación de streamlit

---
+++ Aplicación/app.py
@@ -0,0 +1,68 @@
+import openpyxl
+import numpy as np
+import streamlit as st
+from funciones import *
+
+# Header y descripción
+with st.container():
+    st.title('Traducción de textos')
+    st.markdown('App para traducir testimonios al inglés de forma automática')
+
+# Componente para subir archivo
+archivo = st.file_uploader('Proporciona el archivo a leer:')
+
+# Comprobar si se ha subido un archivo
+if archivo is not None:
+    workbook = openpyxl.load_workbook(archivo)
+    hojas = workbook.sheetnames
+    hoja = st.selectbox('Elige hoja del excel',
+                        ['<Selecciona una opción>'] + hojas)
+
+    # Enseñar datos para la hoja seleccionada
+    if hoja != '<Selecciona una opción>':
+        datos = pd.read_excel(archivo, sheet_name=hoja)
+
+        # Muestra los datos
+        st.write('Cinco primeras observaciones de los datos:')
+        st.write(datos.head())
+        columnas = st.multiselect('¿Qué columna(s) quieres traducir?',
+                                  datos.columns
+                                  )
+        st.write('')
+        col1, col2, col3 = st.columns(3)
+        with col1:
+            st.write('')
+
+        with col2:
+            # Botón para empezar traducción
+            traducir = st.button('Traducir testimonios')
+
+            if traducir:
+                with st.spinner('Traduciendo...'):
+                    for i in columnas:
+                        # Traducir y mantener comentarios en blanco
+                        datos.loc[:, i] = datos.loc[:, i].replace(np.nan, 'empty comment')
+                        datos.loc[:, i] = datos.loc[:, i].apply(traduccion)
+                        datos.loc[:, i] = datos.loc[:, i].replace('empty comment', np.nan)
+
+        with col3:
+            st.write('')
+
+        if traducir:
+            # Enseñar resultados
+            st.write(datos.head())
+
+            col1, col2, col3 = st.columns(3)
+            with col1:
+                st.write('')
+
+            with col2:
+                # Permitir descarga de datos
+                datos_xlsx = to_excel(datos)
+                st.download_button(label='📥 Descargar datos',
+                                   data=datos_xlsx,
+                                   file_name='traduccion.xlsx'
+                                   )
+
+            with col3:
+                st.write('')
---
+++ Aplicación/funciones.py
@@ -0,0 +1,29 @@
+from deep_translator import GoogleTranslator
+import pandas as pd
+from io import BytesIO
+
+
+def traduccion(x):
+    """Traduce textos al inglés"""
+    x = GoogleTranslator(source='auto', target='en').translate(x)
+    return x
+
+
+def to_excel(dataframe):
+    """Transforma un pandas DataFrame a excel"""
+    output = BytesIO()
+    writer = pd.ExcelWriter(output, engine='xlsxwriter')
+
+    # Transformar datos a una hoja de excel
+    dataframe.to_excel(writer, index=False, sheet_name='Hoja 1')
+    workbook = writer.book
+    worksheet = writer.sheets['Hoja 1']
+
+    # Especificar formato
+    format1 = workbook.add_format({'num_format': '0.00'})
+    worksheet.set_column('A:A', None, format1)
+
+    # Guardar resultados
+    writer.close()
+    processed_data = output.getvalue()
+    return processed_data
